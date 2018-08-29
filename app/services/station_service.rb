1e16a3825ed03de3ed7d59df5bb0748729aaa50e
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2018-08-29 10:44:54 -0600

    api key working properly

--- app/services/station_service.rb
+++ app/services/station_service.rb
@@ -4,19 +4,19 @@ class StationService
   end
 
   def find_stations_by_zip
-    get_json("/alt-fuel-stations/v1/nearest.json?location=#{@zip}&fuel_type=LPG,ELEC&limit=10&api_key=#{ENV["ALT_FUEL_FINDER_KEY"]}=JSON")
+    get_json("/api/alt-fuel-stations/v1/nearest.json?location=#{@zip}&fuel_type=LPG,ELEC&limit=10&api_key=#{ENV["ALT_FUEL_FINDER_KEY"]}&format=JSON")
   end
 
 
   private
   def conn
-    Faraday.new("https://developer.nrel.gov/api") do |faraday|
+    Faraday.new("https://developer.nrel.gov") do |faraday|
       faraday.adapter(Faraday.default_adapter)
     end
   end
 
   def get_json(url)
+    really = JSON.parse(conn.get(url).body, symbolize_names: true)
     binding.pry
-    JSON.parse(conn.get(url).body, symbolize_names: true)
   end
 end
