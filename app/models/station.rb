e0d8e6c6c213a206a82dd0055734ff354458505e
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2018-08-29 11:01:59 -0600

    feature test passes, other aspects testing poorly, initiating service test

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,9 +1,11 @@
 class Station
+  attr_reader :name, :address, :fuel_type, :distance, :access_times
+  
   def initialize(station_info)
     @name = station_info[:station_name]
     @address = format_address(station_info)
     @fuel_type = station_info[:fuel_type_code]
-    @distance = "#{station_info[:distance] miles}"
+    @distance = "#{station_info[:distance]} miles"
     @access_times = station_info[:access_days_time]
   end
 
